45a41460ea3683ae57a4cf59630aca1a1d84215c
Cristian Apetroaie <apetroaie.cristian1@gmail.com>
2025-01-24 12:31:28 +0200

    Project: delete unused scripts, move camera logic to player_node

--- scripts/player-input.gd
+++ scripts/player-input.gd
@@ -6,17 +6,8 @@ const DEV = true
 
 @onready var camera = get_parent().get_child(1);
 
-func getMousePos() -> Vector2:
-	return get_viewport().get_mouse_position()
-	
-func mouseToWorldCoords() -> void:
-	var mousePos = getMousePos()
-
 func _unhandled_input(event: InputEvent) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
-	#if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
-		#self.rotate_y(-event.relative.x * 0.01)
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -31,6 +22,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -40,12 +32,3 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
  
-	var space_state = get_world_3d().direct_space_state
-	var mousePos = getMousePos()
-	var rayOrigin = camera.project_ray_origin(mousePos)
-	var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
-	var intersection = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd))
-
-	if not intersection.is_empty():
-		var pos = intersection.position
-		$".".look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0,1,0))
---
+++ scripts/player_node.gd
@@ -0,0 +1,28 @@
+extends Node3D
+@onready var camera = $"Camera3D"
+@onready var player = $"Player"
+var initialCameraPos = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	initialCameraPos = camera.position
+
+func getMousePos() -> Vector2:
+	return get_viewport().get_mouse_position()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var space_state = get_world_3d().direct_space_state
+	var mousePos = getMousePos()
+	var rayOrigin = camera.project_ray_origin(mousePos)
+	var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
+	var intersection = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd))
+
+	if not intersection.is_empty():
+		var pos = intersection.position
+		$Player.look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0,1,0))
+	
+	camera.set_position(
+		Vector3(initialCameraPos.x + player.position.x, initialCameraPos.y + player.position.y, initialCameraPos.z + player.position.z)
+		)
+	camera.look_at(player.position)
