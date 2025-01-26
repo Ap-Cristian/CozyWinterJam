7759fda9d2f354a12402692bba90c19fbe210029
Alexandru Olaru <xdxalexandru404@gmail.com>
2025-01-26 21:42:14 +0200

    fire: Add wood lowering into fire animation.

--- scripts/fire.gd
+++ scripts/fire.gd
@@ -8,17 +8,20 @@ extends StaticBody3D
 @onready var fire_particles = $"FireParticles";
 @onready var smoke_particles = $"SmokeParticles";
 
-
+var wood_scene = preload("res://scenes/wood.tscn");
 var nodeHelper = preload("res://scripts/helpers/node_helpers.gd").new();
 
 const PLAYER_TO_FIRE_MIN_DISTNACE = 10;
 const WARMNESS_PER_WOOD = 0.3;
 const COLD_MODIFIER_INCREASE_PER_DEPOSIT = 0.003;
+const WOOD_LOWERING_SPEED_INTO_FIRE = 3;
 
 var fire_strength = 1.0;
 var decrease_modifier = 0.015;
 var dead = false;
 var can_deposit = false;
+
+var animated_wood = null;
 
 signal fire_strength_updated
 signal fire_interactable_updated
@@ -45,6 +48,12 @@ func _input(event: InputEvent):
 			fire_interactable_updated.emit(false, 0);
 			decrease_modifier += COLD_MODIFIER_INCREASE_PER_DEPOSIT;
 
+			var obj = wood_scene.instantiate();
+			obj.position = Vector3(self.position.x, self.position.y - 4, self.position.z);
+			player_node.add_child(obj);
+			animated_wood = obj;
+			animated_wood.get_child(0).get_child(1).disabled = true;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fire_strength_updated.connect(ui.update_fire_strength);
@@ -68,6 +77,15 @@ func _process(delta: float) -> void:
 			die();
 		return;
 	
+	if animated_wood != null:
+		if animated_wood.position.y < self.position.y - 6.5:
+			animated_wood.queue_free();
+			animated_wood = null;
+		else:
+			animated_wood.position.y = move_toward(animated_wood.position.y, self.position.y - 7, WOOD_LOWERING_SPEED_INTO_FIRE * delta);
+			var scale = move_toward(animated_wood.scale.x, 0, WOOD_LOWERING_SPEED_INTO_FIRE * delta);
+			animated_wood.scale = Vector3(scale, scale, scale);
+
 	fire_strength_updated.emit(fire_strength);
 	get_child(2).light_energy = 2 + 130 * fire_strength;
 	audio_player.volume_db = -10 - 30 * (1 - fire_strength) - distance_between_player_and_fire();
